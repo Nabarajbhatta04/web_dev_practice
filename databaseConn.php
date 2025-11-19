25f308bd4d9dae87d8a744aea01eb3b74034ab2d
Naresh Bhatta <113630862+Nerrybhatt@users.noreply.github.com>
2025-11-19 22:17:26 +0545

    some change is made

--- databaseConn.php
+++ databaseConn.php
@@ -6,11 +6,8 @@ $database = "website_meme";
 $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("database is not connected!" . mysqli_connect_error());
-} else {
-    echo "connection successfully!";
 }
-
-
+;
 $search = "";
 if (isset($_GET['search'])) {
     $search = mysqli_real_escape_string($conn, $_GET['search']);
@@ -26,6 +23,7 @@ if (isset($_GET['search'])) {
 
 < <form action="" method="GET">
     <input type="text" name="search" placeholder="Search memes..." value="<?php echo $search; ?>">
+
     <button type="submit">Search</button>
     </form>
 
