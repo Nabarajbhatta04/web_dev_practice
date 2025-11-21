34a203a0443e26d2ffd83618f34b8c0038413a8d
Naresh Bhatta <113630862+Nerrybhatt@users.noreply.github.com>
2025-11-21 22:24:06 +0545

    data inserted into the database from form

--- form_db.php
+++ form_db.php
@@ -9,14 +9,14 @@
 
 <body>
     <form method="POST">
-        <label>Name:</label><input type="text" name="uname" placeholder="Enter user name!"></br>
-        <label>Adderss:</label><input type="text" name="uaddress" placeholder="Enter user address!"></br>
-        <label>Number:</label><input type="number" name="unumber"></br>
-        <label>Email:</label><input type="email" name="uemail" placeholder="Email"></br>
+        <label>Name:</label><input type="text" name="name" placeholder="Enter user name!"></br>
+        <label>Adderss:</label><input type="text" name="address" placeholder="Enter user address!"></br>
+        <label>Number:</label><input type="number" name="number"></br>
+        <label>Email:</label><input type="email" name="email" placeholder="Email"></br>
         <label>Gender:</label></br>
-        <input type="radio" name="gender" value="umale">Male</br>
-        <input type="radio" name="gender" value="ufemale">Female</br>
-        <input type="radio" name="gender" value="uothe">other</br>
+        <input type="radio" name="gender" value="male">Male</br>
+        <input type="radio" name="gender" value="female">Female</br>
+        <input type="radio" name="gender" value="other">other</br>
         <label>Hobbies:</label></br>
         <select name="hobbies[]" multiple size="4" required>
             <option value="playing">Playing</option>
@@ -25,12 +25,13 @@
             <option value="singing">singing</option>
 
         </select>
+        <input type="submit" name="submit" value="submit">
     </form>
 
     <?php
     include "databaseConnection.php";
 
-    $sql = "create table introduction (
+    $sql = "create table user_introduction (
                                       id INT PRIMARY KEY AUTO_INCREMENT,
                                       name VARCHAR(20),
                                       address  VARCHAR(30),
@@ -39,12 +40,42 @@
                                       gender VARCHAR(20),
                                       hobbies VARCHAR(40)
                                     )";
-    if ($sql) {
-        echo "data inserted successfully!";
+
+    // $table = mysqli_query($conn, $sql);
+    // if ($table) {
+    //     echo "table created successfully!";
+    // }
+    
+
+
+
+
+
+    if (isset($_POST['submit'])) {
+
+        $name = $_POST['name'];
+        $address = $_POST['address'];
+
+        $number = $_POST['number'];
+        $email = $_POST['email'];
+        $gender = $_POST['gender'];
+        // Hobbies is an array -> convert into string
+        $hobbies = implode(", ", $_POST['hobbies']);
+
+        $insert = "insert into user_introduction (name,address,number,email,gender,hobbies) values
+        ('$name','$address',$number,'$email','$gender','$hobbies')";
+
+        $result = mysqli_query($conn, $insert);
+        if ($result) {
+            echo "Data inserted successfully!";
+        } else {
+            echo "Insert failed: " . mysqli_error($conn);
+        }
+
     }
 
-    //     $insert= "insert into introduction (name,address,number,email,gender,hobbies) values
-    //                                         ()"
+
+
     ?>
 
 </body>
